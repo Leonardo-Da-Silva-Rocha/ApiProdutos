2a93657982e43d7b13dde9aab89ca6291c094abb
Leonardo-Da-Silva-Rocha <leosilvarocha.lsr@gmail.com>
2021-10-31 15:39:07 -0300

    implementando a api de produtos

--- ApiProdutos/Data/AppDbContext.cs
+++ ApiProdutos/Data/AppDbContext.cs
@@ -18,6 +18,14 @@ namespace ApiProdutos.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Produto>()
                 .HasData(
                     new Produto { Id = 1, Nome = "Caderno", Preco = 7.95M, Estoque = 10},
                     new Produto { Id = 2, Nome = "Borracha", Preco = 2.45M, Estoque = 30 },
--- ApiProdutos/Startup.cs
+++ ApiProdutos/Startup.cs
@@ -1,5 +1,7 @@
+using ApiProdutos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,10 @@ namespace ApiProdutos
         public void ConfigureServices(IServiceCollection services)
         {
 
+            string mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContextPool<AppDbContext>(options =>
+                 options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
